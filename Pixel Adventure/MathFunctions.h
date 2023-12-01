f5ec372470273c07442e013ba4138888c1b57a80
WedrownyElite <60861809+WedrownyElite@users.noreply.github.com>
2023-12-01 01:08:27 -0600

    Update MathFunctions.h

--- Pixel Adventure/MathFunctions.h
+++ Pixel Adventure/MathFunctions.h
@@ -6,5 +6,6 @@ class MathFunctions {
 public:
 	static float angleDifference(float angle_1, float angle_2);
 	static olc::vf2d GetWorldMousePos(olc::TileTransformedView& tv, olc::PixelGameEngine* pge);
+	olc::vf2d GetPlayerPos(olc::TileTransformedView& tv, olc::PixelGameEngine* pge, olc::vf2d PlayerPos);
 	static float PointTo(olc::vf2d pos1, olc::vf2d pos2);
 };
