2a91a9c521b9709c62b89bd956b2c5dc7098d57f
WedrownyElite <chris.sliwinski@gmail.com>
2023-12-01 02:15:15 -0600

    Brh

--- Pixel Adventure/Player.cpp
+++ Pixel Adventure/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 #include "GlobalVars.h"
 
+#include "olcPGEX_Animator2D.h"
 
 void Player::PlayAnimation(olc::TileTransformedView& tv, olc::PixelGameEngine* pge, olc::vf2d PlayerPos, float fElapsedTime) {
 	if (DrawAnim == false && pge->GetKey(olc::Key::A).bHeld) {
